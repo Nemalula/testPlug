f3df5d8f950e0ef7e146e2c3ac348119c437e17c
Nemalula <baaaariel@gmail.com>
2019-04-17 19:05:57 +0300

    Message

--- src/main/java/io/github/something/Citah.java
+++ src/main/java/io/github/something/Citah.java
@@ -19,24 +19,23 @@ public final class Citah extends JavaPlugin {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        Player player = (Player)sender;
-        if(label.equalsIgnoreCase("msg")) {
+        Player player = (Player) sender;
+        if (label.equalsIgnoreCase("msg")) {
             if (args.length != 0) {
-                if(Bukkit.getPlayer(args[0])instanceof Player){
+                if (Bukkit.getPlayer(args[0]) instanceof Player) {
                     Bukkit.getPlayer(args[0]).sendMessage("hi");
                     return true;
-                }
-                else{
-                    player.sendMessage("failed to recognize your existance");
+                } else {
+                    player.sendMessage("failed to recognizre your existance");
                     return true;
                 }
-            }
-            else{
+            } else {
                 player.sendMessage("pls enter a nem");
                 return true;
             }
         }
         return false;
-        //tbh i always forget how to do this part in idea. xd
     }
 }
+
+
